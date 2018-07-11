e994cd9a73e5da25e850b39f02b2663ddbfc96d5
leejh3224 <leejh3224@naver.com>
2018-07-11 17:05:20 +0900

    2018.07.11

---
+++ manasa-and-stones/solution.cpp
@@ -0,0 +1,44 @@
+/*
+ * What you should know is that although number of permutations for possible difference is 2^(n-1), 
+ * your goal is to get only different values among them.
+ * 
+ * So if possible diffs are [2,3,3,4], all you have to return is [2,3,4].
+ * 
+ * Here is an example of getting an answer when n = 4, a = 4, b = 5.
+ * 
+ * 4 4 4 = 12 // a + a + a
+ * 4 4 5 = 13 // a + a + b
+ * 4 5 5 = 14 // a + b + b
+ * 5 5 5 = 15 // b + b + b
+ * 
+ * You can find out that number of answers would be n and 
+ * you can get those values by increasing the number of b in sum.
+ * 
+ * And you should be aware that answers must be sorted in ascending order and there are some edge cases like when n = 1 or a = b.
+ * Here is solution for c++ users.
+ */
+vector<int> stones(int n, int a, int b)
+{
+  vector<int> answer;
+
+  if (n == 1)
+  {
+    answer.push_back(0);
+    return answer;
+  }
+
+  if (a == b)
+  {
+    answer.push_back((n - 1) * a);
+    return answer;
+  }
+
+  for (int i = 0; i < n; i++)
+    answer.push_back((n - i - 1) * a + i * b);
+
+  sort(answer.begin(), answer.end(), [](int x, int y) {
+    return x < y;
+  });
+
+  return answer;
+}
